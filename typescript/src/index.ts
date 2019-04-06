7eb294ac7cd1b625c2c07fbacfb9ca7480dd7656
kuangjiajia <1298201366@qq.com>
2019-04-06 21:30:28 +0800

    add some demo

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -197,12 +197,380 @@
 // console.log(reverse(x))
 
 
-function getLength(something: string | number): number {
-  if (typeof something === "string") {
-    return something.length
-  } else {
-    return Number(something).toString().length
-  }
+// function getLength(something: string | number): number {
+//   if (typeof something === "string") {
+//     return something.length
+//   } else {
+//     return Number(something).toString().length
+//   }
+// }
+
+// console.log("length: " + getLength(999888))
+
+
+// function getL≠ength(str: string | number): number {
+//   if((<string>str).length) {
+//     return (<string>str).length
+//   }else {
+//     return Number(str).toString().length
+//   }
+// } 
+
+// let a: number[] = [1, 2, 3, 4]
+// let ro: ReadonlyArray<number> = [1, 2, 3]
+
+// a = ro as number[]
+// console.log(a)
+
+// interface SquareConfig {
+//   color?: string;
+//   width?: number;
+// }
+
+// function createSquare(config: SquareConfig): { color: string; area: number } {
+//   let newSquare = { color: "white", area: 100 };
+//   if (config.color) {
+//     newSquare.color = config.color;
+//   }
+//   if (config.width) {
+//     newSquare.area = config.width * config.width;
+//   }
+//   return newSquare;
+// }
+
+// let mySquare = createSquare({ colour: "black" } as SquareConfig);
+// console.log(mySquare)
+
+// class Animal {
+//   private name: string;
+//   constructor(theName: string) { this.name = theName; }
+// }
+
+// class Rhino extends Animal {
+//   constructor() { super("Rhino"); }
+// }
+
+// let animal = new Animal("Goat");
+// let rhino = new Rhino();
+
+// console.log(rhino)
+
+// interface Card {
+//   suit: string
+//   card: number
+// }
+
+// interface Deck {
+//   suits: string[]
+//   cards: number[]
+//   createCardPicker(this: Deck): () => Card
+// }
+
+
+// let deck = {
+//   suits: ["hearts", "spades", "clubs", "diamonds"],
+//   cards: Array(52),
+//   createCardPicker: function (this: Deck) {
+//     return () => {
+//       let pickedCard = Math.floor(Math.random() * 52);
+//       let pickedSuit = Math.floor(pickedCard / 13);
+
+//       return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
+//     }
+//   }
+// }
+
+// let cardPicker = deck.createCardPicker();
+// let pickedCard = cardPicker();
+
+// // alert("card: " + pickedCard.card + " of " + pickedCard.suit);
+// console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+
+// const typed = document.getElementById("typed");
+// if (typed) {
+//   const pay = function (current: number, discount: number): number {
+//     return current * (discount / 100);
+//   }
+//   typed.addEventListener("click", function (this: HTMLElement, ev: MouseEvent) {
+//     console.log(pay(1000, 20))
+//   });
+// }
+
+
+// class UI {
+//   name: string
+//   constructor(name) {
+//     this.name = name
+//   }
+//   addEventListener(type: string, handler: (this: UI, e: string) => void) {
+//     handler.call(this, this.name)
+//   }
+// }
+
+// const ui = new UI("kuangjiajia")
+// const onClick = ui.addEventListener("click", function (e: string) {
+//   console.log(this.name)
+// })
+
+// class World {
+//   private max: number
+
+//   country: string
+//   constructor(country: string) {
+//     this.country = country
+//     this.max = 100
+//   }
+
+//   output() {
+//     return this.country
+//   }
+// }
+
+// class Country extends World {
+//   constructor(country: string) {
+//     super(country)
+//   }
+// }
+
+// let china = new Country("china")
+// console.log(china)
+
+
+// class staticClass {
+//   static sName = "kuangjiajia"
+//   static sFunc = (): string => staticClass.sName
+// }
+
+// console.log(staticClass.sFunc())
+
+
+// class Employee {
+//   private _fullName: string
+
+//   constructor(_fullName: string) {
+//     this._fullName = _fullName
+//   }
+
+//   get fullName(): string {
+//     return this._fullName
+//   }
+
+//   set fullName(newName: string) {
+//     this._fullName = newName
+//   }
+// }
+
+// let worker = new Employee("kuangjiajia")
+// worker.fullName = "jiajiakuang"
+
+
+// abstract class Department {
+//   name: string
+//   constructor(name: string) {
+//     this.name = name
+//   }
+
+//   printName(): void {
+//     console.log(`Department name is ${this.name}`)
+//   }
+
+//   abstract printMeeting(): void
+// }
+
+
+// class AccountingDepartment extends Department {
+//   meeting: string
+//   constructor(name: string, meeting: string) {
+//     super(name)
+//     this.meeting = meeting
+//   }
+
+//   printMeeting(): void {
+//     console.log(`this meeting is ${this.meeting}`)
+//   }
+// }
+
+
+// let kjj = new AccountingDepartment("kuangjiajia", "redrock")
+
+// kjj.printMeeting()
+// kjj.printName()
+
+
+// class XDom {
+//   static xdom: XDom | null
+//   static shareInstance = (): XDom => {
+//     if (!XDom.xdom) {
+//       XDom.xdom = new XDom()
+//     }
+//     return XDom.xdom;
+//   }
+
+//   public dom: HTMLElement | null
+
+//   constructor() {
+//     this.dom = document.getElementById("root")
+//   }
+
+//   insert(text: string) {
+//     if (this.dom) {
+//       this.dom.innerText = text
+//     }
+//   }
+
+//   setColor(color: string) {
+//     if (this.dom) {
+//       this.dom.style.color = color
+//     }
+//   }
+// }
+
+// let xdom = new XDom()
+// xdom.insert("kuangjiajia")
+// xdom.setColor("pink")
+
+
+
+
+// function sum({x,y}: {x:number,y:number}): number {
+//   return x + y
+// }
+
+// interface ISum {
+//   x: number
+//   y: number
+// }
+
+// function sum({ x, y }: ISum): number {
+//   return x + y
+// }
+
+// console.log(sum({ x: 1, y: 2 }))
+
+
+// interface Test {
+//   readonly x: string
+//   readonly y: string
+// }
+
+
+// let test: Test = {
+//   x: "kjj",
+//   y: "lx"
+// }
+
+// console.log(test)
+
+
+// interface ICheckPoint {
+//   x: string
+//   y: string
+// }
+
+// function create(config: ICheckPoint): void {
+//   console.log(config)
+// }
+
+// create({
+//   x: "x",
+//   y: "y",
+//   zz: "zzx"
+// } as ICheckPoint)
+
+// interface A {
+//   x: string
+// }
+
+
+// interface B extends A {
+//   y: string
+// }
+
+// interface IFunc {
+//   sum: (x: number, y: number) => number
+// }
+
+// const d: IFunc = {
+//   sum(x: number, y: number): number {
+//     return x + y
+//   }
+// }
+
+
+// interface iArr {
+//   [index: number]: string
+// }
+
+// let x: iArr = ["zzx", "lx"]
+
+// console.log(x)
+// console.log(x[0])
+
+// type printType = <T>(args: T) => T
+// function print<T>(args: T): T {
+//   return args
+// }
+
+// console.log(print("kuangjiajia"))
+
+// const _p: printType = print
+
+
+// function extend<T, U>(first: T, second: U): T & U {
+//   let result = <T & U>{}
+//   for (let id in first) {
+//     (<any>result)[id] = (<any>first)[id]
+//   }
+//   for (let id in second) {
+//     if (!result.hasOwnProperty(id)) {
+//       (<any>result)[id] = (<any>second)[id]
+//     }
+//   }
+//   return result
+// }
+
+// class Person {
+//   name: string
+//   constructor(name: string) {
+//     this.name = name
+//   }
+// }
+
+// interface Loggable {
+//   log(name: string): void
+// }
+
+// class ConsoLog implements Loggable {
+//   log(name: string): void {
+//     console.log(name)
+//   }
+// }
+
+// let newObj = extend(new Person("kuangjiajia"), new ConsoLog())
+
+// console.log(newObj)
+
+// function sumStr(input: number | string): string {
+//   if (typeof input === "string") {
+//     return `string: ${input}`
+//   } else if (typeof input === "number") {
+//     return `number: ${input}`
+//   } else {
+//     throw new Error("input error type")
+//   }
+// }
+
+// console.log(sumStr("kuangjiajia"))
+
+
+
+function pluck<T, K extends keyof T>(o: T, names: K[]): T[K][] {
+  return names.map(n => o[n]);
 }
 
-console.log("length: " + getLength(999888))
+const d = pluck({
+  d: "123"
+}, ["d"])
+
+console.log(d)
