691940c7e2ac262792af38356aa26e13374c7d89
kuangjiajia <1298201366@qq.com>
2019-04-12 21:39:26 +0800

    add vscode

--- vscode-test/index.js
+++ vscode-test/index.js
@@ -1,8 +1,38 @@
 let str = "this is a string"
 function foo() {
   console.log(str)
-
+  test()
   return 123
 }
 
+
+function test() {
+  kuangjiajia()
+  return "kuangjiajia"
+}
+
 foo()
+
+
+
+function kuangjiajia() {
+  console.log("this is a test")
+}
+
+
+class Person {
+  //constructor
+  constructor(name) {
+    this.name = name
+  }
+}
+
+class Student extends Person {
+  constructor(name, age) {
+    super(name)
+    this.age = age
+  }
+}
+
+let kuangjiajia2 = new Person("kuangjiajia", 30)
+console.log(kuangjiajia2)
